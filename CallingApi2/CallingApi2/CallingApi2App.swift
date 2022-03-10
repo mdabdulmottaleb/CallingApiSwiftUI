2e0262edec0e618ba61f901c8ef704eaeec34006
MdAbdulMottaleb <61162466+mdabdulmottaleb@users.noreply.github.com>
2022-03-10 09:14:36 +0600

    Add files via upload

---
+++ CallingApi2/CallingApi2/CallingApi2App.swift
@@ -0,0 +1,17 @@
+//
+//  CallingApi2App.swift
+//  CallingApi2
+//
+//  Created by MacBook Pro on 9/2/22.
+//
+
+import SwiftUI
+
+@main
+struct CallingApi2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
